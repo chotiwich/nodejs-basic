587a975d21ba2d33317665af2a558c85140bbd92
chotiwich <47513105+chotiwich@users.noreply.github.com>
2019-09-21 12:21:36 +0700

    admit

---
+++ 1-const-let-var.js
@@ -0,0 +1,17 @@
+// const a = "a";
+// a = "b";
+
+let a = "a";
+a = "b";
+console.log(a);
+
+//var a = "a";
+//a = "c";
+c; //onsole.log(a);
+
+let a = "a";
+{
+  let a = "b";
+  console.log(1, a);
+}
+console.log(2, a);
